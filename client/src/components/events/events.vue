b88b13f43bc73d943d0f36cc3a9e26af64d5bc8d
Raed Fadhlaoui <fadhlaouiraied@hotmail.com>
2021-01-16 14:28:05 +0100

    mergin dheker work

--- client/src/components/events/events.vue
+++ client/src/components/events/events.vue
@@ -1,111 +1,89 @@
 <template>
-  <div>
-    <transition-group name="fade" tag="div">
-      <div v-for="i in [currentIndex]" :key="i">
-        <img :src="currentImg" />
-      </div>
-    </transition-group>
-    <a class="prev" @click="prev" href="#">&#10094; Previous</a>
-    <a class="next" @click="next" href="#">&#10095; Next</a>
-  </div>
- 
+  <v-container fluid>
+    <!-- event -->
+    <v-row justify="space-around">
+      <v-col cols="3">
+        <div v-for="event in events" :key="event.id">
+          <div>
+            <h3>{{event.title}}</h3>
+          </div>
+          <v-img :src="event.image" aspect-ratio="1.7"></v-img>
+          <br />
+          <p>
+            {{event.description}}
+            <strong>{{event.date}}</strong>
+          </p>
 
+          <div class="text-center">
+            <div class="my-2">
+               <a :href="event.youtubeLink"  target="_blank">
+              <v-btn depressed color="primary">
+              watch video
+              </v-btn>
+               </a>
+              <v-btn depressed color="primary">Buy Ticket</v-btn>
+            </div>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
 
+    <!-- Plan a Visit to Tunisia -->
+    <v-divider></v-divider>
+
+    <h2 data-orig-font="48px" style="font-size: 40px;">Plan a Visit to Tunisia</h2>
+
+    <p>
+      Participating in one of the many festivals in Tunisia is a great way to experience Tunisia.
+      If you are interested in visiting Tunisia our travel experts
+      are happy to assist you to plan a Tunisia tour that could include one or more of these festivals in Tunisia.
+    </p>
+  </v-container>
 </template>
-
 
 
 <script>
 export default {
-  name: "events",
+  name: "Events",
+  props: {},
+  method: {
+    WatchVideo() {},
+  },
   data() {
     return {
-      images: [
-        "https://cdn.pixabay.com/photo/2015/12/12/15/24/amsterdam-1089646_1280.jpg",
-        "https://cdn.pixabay.com/photo/2016/02/17/23/03/usa-1206240_1280.jpg",
-        "https://cdn.pixabay.com/photo/2015/05/15/14/27/eiffel-tower-768501_1280.jpg",
-        "https://cdn.pixabay.com/photo/2016/12/04/19/30/berlin-cathedral-1882397_1280.jpg"
+      events: [
+        {
+          id: 1,
+          title: "International Festival of Sousse",
+          image:
+            "https://scontent.ftun2-1.fna.fbcdn.net/v/t1.0-9/117099366_1036949290110505_2677540095970272956_o.jpg?_nc_cat=106&ccb=2&_nc_sid=0debeb&_nc_ohc=Kxzs30d-KdkAX9nJx3b&_nc_oc=AQmrLVw1Y3IVTvQAT3Y0ne-wJVRR61ihuYtbmalIWyEcR0yfzl00I3TjZMsQ9QomDCU&_nc_ht=scontent.ftun2-1.fna&oh=12c1f36490749c483aa8a838bd6c77d5&oe=60235378",
+          description: `International Festival of Sousse serves to add even more to the mix,
+            and this year marks the 47th edition of the festival.
+            Tunisian musicians and artists dominate the lineup at the festival,
+            and performers from neighboring countries as well as Romania, Italy, Belgium, Syria and Lebanon add their artistic stylings into the mix,
+            and Russia is also sending its Symphonic Orchestra across to Tunisia.`,
+          date: "August 13",
+          youtubeLink:
+            "https://www.youtube.com/watch?v=1dn60yPZG3A&ab_channel=MercifulServant",
+        },
+        {
+          id: 2,
+          title: "International Festival of Sousse",
+          image:
+            "https://scontent.ftun2-1.fna.fbcdn.net/v/t1.0-9/117099366_1036949290110505_2677540095970272956_o.jpg?_nc_cat=106&ccb=2&_nc_sid=0debeb&_nc_ohc=Kxzs30d-KdkAX9nJx3b&_nc_oc=AQmrLVw1Y3IVTvQAT3Y0ne-wJVRR61ihuYtbmalIWyEcR0yfzl00I3TjZMsQ9QomDCU&_nc_ht=scontent.ftun2-1.fna&oh=12c1f36490749c483aa8a838bd6c77d5&oe=60235378",
+          description: `International Festival of Sousse serves to add even more to the mix,
+            and this year marks the 47th edition of the festival.
+            Tunisian musicians and artists dominate the lineup at the festival,
+            and performers from neighboring countries as well as Romania, Italy, Belgium, Syria and Lebanon add their artistic stylings into the mix,
+            and Russia is also sending its Symphonic Orchestra across to Tunisia.`,
+          date: "August 13",
+          youtubeLink:
+            "https://www.youtube.com/watch?v=U02uHaQunD4&ab_channel=DirtyWaterMedia",
+        },
       ],
-      timer: null,
-      currentIndex: 0
     };
   },
-
-  mounted: function() {
-    this.startSlide();
-  },
-
-  methods: {
-    startSlide: function() {
-      this.timer = setInterval(this.next, 4000);
-    },
-
-    next: function() {
-      this.currentIndex += 1;
-    },
-    prev: function() {
-      this.currentIndex -= 1;
-    }
-  },
-
-  computed: {
-    currentImg: function() {
-      return this.images[Math.abs(this.currentIndex) % this.images.length];
-    }
-  }
 };
 </script>
 
-<style>
 
-.fade-enter-active,
-.fade-leave-active {
-  transition: all 0.9s ease;
-  overflow: hidden;
-  visibility: visible;
-  position: absolute;
-  width:100%;
-  opacity: 1;
-}
-
-.fade-enter,
-.fade-leave-to {
-  visibility: hidden;
-  width:100%;
-  opacity: 0;
-}
-
-img {
-  height:600px;
-  width:100%
-}
-
-.prev, .next {
-  cursor: pointer;
-  position: absolute;
-  top: 40%;
-  width: auto;
-  padding: 16px;
-  color: white;
-  font-weight: bold;
-  font-size: 18px;
-  transition: 0.7s ease;
-  border-radius: 0 4px 4px 0;
-  text-decoration: none;
-  user-select: none;
-}
-
-.next {
-  right: 0;
-}
-
-.prev {
-  left: 0;
-}
-
-.prev:hover, .next:hover {
-  background-color: rgba(0,0,0,0.9);
-}
-
-
-</style>
